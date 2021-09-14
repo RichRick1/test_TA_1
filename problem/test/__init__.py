fef2dc8f73835f1d3633254e9f3d56f7c76f00d3
RichRick1 <36164007+RichRick1@users.noreply.github.com>
2021-09-14 16:39:56 -0400

    Add files via upload

---
+++ problem/test/__init__.py
@@ -0,0 +1,1 @@
+r"Testing init."
---
+++ problem/test/test_finding_root.py
@@ -0,0 +1,21 @@
+from ..finding_root import *
+
+import numpy as np
+from numpy.testing import assert_equal, assert_allclose
+
+def test_f():
+    """
+    tesring f(x) function
+    :return:
+    """
+    assert_allclose(1, f(1))
+    assert_allclose(2, f(8))
+    assert_allclose(-1, f(-1))
+
+
+def test_exponential_function():
+    """
+    testing exponential function
+    :return:
+    """
+    assert_allclose(-0.5, exponential_function(0))
